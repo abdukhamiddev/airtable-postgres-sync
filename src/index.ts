0822472230d58cffce188c08b2b0799692905e35
abdukhamiddev <latipovabdukhamid@gmail.com>
2023-08-05 22:55:59 +0500

    third commit

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 import axios, { AxiosResponse } from "axios";
 import pg, { Pool } from "pg";
-import { isEqual } from 'lodash';
-
+import { isEqual } from "lodash";
+import express from "express";
 
 interface WebhookSpecification {
   options: {
     filters: {
       dataTypes: string[];
-   
     };
   };
 }
-
 
 async function fetchTables(token: string, baseID: string): Promise<any[]> {
   try {
@@ -48,10 +46,9 @@ async function fetchTableRecords(
 
 async function createPostgresTable(
   pgURL: string,
-  tables:any[],
+  tables: any[],
   tableName: string
 ) {
-  
   if (!tables) {
     console.log("Error fetching tables");
     return;
@@ -71,10 +68,11 @@ async function createPostgresTable(
     }
 
     const columnDefinitions = fields.map((field: any) => {
-      const columnName =
-        field.name === "Unit cost" ? `"${field.name}"` : `"${field.name}"`;
+      const columnName = `"${field.name}"`;
       return `${columnName} ${mapAirtableTypeToPostgresType(field.type)}`;
     });
+
+    columnDefinitions.push(`"airtable_record_id" TEXT PRIMARY KEY`);
 
     const createTableQuery = `CREATE TABLE IF NOT EXISTS "${tableName}" (${columnDefinitions.join(
       ", "
@@ -91,29 +89,30 @@ async function createPostgresTable(
   }
 }
 
-async function updatePostgresTable(pgURL: string, payload: any,token:string,baseID:string) {
-  
+async function updatePostgresTable(
+  pgURL: string,
+  payload: any,
+  token: string,
+  baseID: string
+) {
   const pool = new Pool({ connectionString: pgURL, ssl: true });
-    const client = await pool.connect();
+  const client = await pool.connect();
   try {
-    
-
     const lastPayload = payload.payloads[payload.payloads.length - 1];
     const changedTablesById = lastPayload.changedTablesById;
-    console.log(lastPayload)
-
+    console.log(lastPayload);
 
     if (changedTablesById) {
       const tableId = Object.keys(changedTablesById)[0];
       const changedRecordsById = changedTablesById[tableId].changedRecordsById;
-      const tables = await fetchTables(token,baseID)
+      const tables = await fetchTables(token, baseID);
 
-      const table = tables.find((t)=>t.id === tableId)
+      const table = tables.find((t) => t.id === tableId);
 
       if (changedRecordsById) {
         const recordId = Object.keys(changedRecordsById)[0];
-        const changedFieldsById = changedRecordsById[recordId].changedFieldsById;
-
+        const changedFieldsById =
+          changedRecordsById[recordId].changedFieldsById;
 
         if (changedFieldsById) {
           const fieldId = Object.keys(changedFieldsById)[0];
@@ -131,10 +130,10 @@ async function updatePostgresTable(pgURL: string, payload: any,token:string,base
         }
       }
 
-
       if (changedRecordsById) {
         const recordId = Object.keys(changedRecordsById)[0];
-        const currentValues = changedRecordsById[recordId].current.cellValuesByFieldId;
+        const currentValues =
+          changedRecordsById[recordId].current.cellValuesByFieldId;
 
         for (const fieldId in currentValues) {
           const newFieldName = currentValues[fieldId];
@@ -150,8 +149,10 @@ async function updatePostgresTable(pgURL: string, payload: any,token:string,base
       }
 
       if (changedTablesById[tableId].changedMetadata) {
-        const newTableName = changedTablesById[tableId].changedMetadata.current.name;
-        const oldTableName = changedTablesById[tableId].changedMetadata.previous.name;
+        const newTableName =
+          changedTablesById[tableId].changedMetadata.current.name;
+        const oldTableName =
+          changedTablesById[tableId].changedMetadata.previous.name;
 
         const updateTableNameQuery = `
           ALTER TABLE "${oldTableName}"
@@ -162,16 +163,12 @@ async function updatePostgresTable(pgURL: string, payload: any,token:string,base
         console.log(`Table name updated to: ${newTableName}`);
       }
     }
-
-
-
   } catch (error) {
     console.error("Error updating PostgreSQL table:", error);
-  }finally{
-  client.release()
+  } finally {
+    client.release();
   }
 }
-
 
 async function insertRecordsToPostgres(
   token: string,
@@ -179,20 +176,19 @@ async function insertRecordsToPostgres(
   tableName: string,
   pgURL: string
 ) {
-  
   const pool = new Pool({ connectionString: pgURL, ssl: true });
   const client = await pool.connect();
 
   try {
-    const tableRecords = await fetchTableRecords(token,baseId,tableName);
+    const tableRecords = await fetchTableRecords(token, baseId, tableName);
     if (tableRecords.length === 0) {
       console.log("No records to insert.");
       return;
     }
 
     for (const record of tableRecords) {
-      const fieldNames = Object.keys(record.fields);
-      const fieldValues = Object.values(record.fields);
+      const fieldNames = [...Object.keys(record.fields), "airtable_record_id"];
+      const fieldValues = [...Object.values(record.fields), record.id];
 
       const formattedValues = fieldValues.map((value, index) => {
         const fieldName = fieldNames[index];
@@ -218,6 +214,7 @@ async function insertRecordsToPostgres(
         .map((name) => `"${name}"`)
         .join(", ")})
         VALUES (${formattedValues.join(", ")})
+        ON CONFLICT ("airtable_record_id") DO NOTHING;
       `;
 
       await client.query(insertQuery);
@@ -230,6 +227,26 @@ async function insertRecordsToPostgres(
     client.release();
   }
 }
+
+async function createAllTablesAndInsertRecords(
+  token: string,
+  baseID: string,
+  pgURL: string
+) {
+  try {
+    const tables: any[] = await fetchTables(token, baseID);
+
+    for (const table of tables) {
+      const tableName = table.name;
+
+      await createPostgresTable(pgURL, tables, tableName);
+      await insertRecordsToPostgres(token, baseID, tableName, pgURL);
+    }
+  } catch (error) {
+    console.log("Conflict with creating tables and inserting Records", error);
+  }
+}
+
 async function fetchWebhooks(baseID: string, token: string): Promise<any> {
   const response: AxiosResponse<any> = await axios.get(
     `https://api.airtable.com/v0/bases/${baseID}/webhooks`,
@@ -296,7 +313,11 @@ async function createWebhook(
     }
   }
 }
-async function fetchWebhookPayload(baseID: string, token: string, webhookID: string): Promise<any[]> {
+async function fetchWebhookPayload(
+  baseID: string,
+  token: string,
+  webhookID: string
+): Promise<any[]> {
   try {
     const response: AxiosResponse = await axios.get(
       `https://api.airtable.com/v0/bases/${baseID}/webhooks/${webhookID}/payloads`,
@@ -305,84 +326,120 @@ async function fetchWebhookPayload(baseID: string, token: string, webhookID: str
       }
     );
 
-    return response.data.payloads || [];
+    return response.data;
   } catch (error: any) {
     console.error("Error fetching webhook payload:", error.message);
     return [];
   }
 }
-
 let uniqueWebhookCount = 0;
-  
-  async function manageWebhooks(
-    webhookSpecifications: WebhookSpecification[],
-    notificationURL: string,
-    token: string,
-    baseID: string
-  ): Promise<any> {
-    try {
-      const webhooks = await fetchWebhooks(baseID, token);
-      const webhookResults: any[] = [];
-      const existingWebhookSpecifications = new Set<string>();
-  
-      for (const webhook of webhooks.webhooks || []) {
-        existingWebhookSpecifications.add(JSON.stringify(webhook.specification));
-      }
-  
-      for (const webhookSpecification of webhookSpecifications) {
-        if (uniqueWebhookCount > 3) {
-          console.log("Reached the maximum limit of three webhooks. Skipping creation and refreshing.");
-          await refreshExistingWebhooks(webhooks, webhookSpecifications, baseID, token);
-          break;
-        }
-  
-        if (existingWebhookSpecifications.size <= 3) {
-          if (!existingWebhookSpecifications.has(JSON.stringify(webhookSpecification))) {
-            const createRes = await createWebhook(
-              baseID,
-              token,
-              notificationURL,
-              webhookSpecification
-            );
-            webhookResults.push(createRes);
-            existingWebhookSpecifications.add(JSON.stringify(webhookSpecification));
-            uniqueWebhookCount++;
-          } else {
-            console.log(`Webhook with specification ${JSON.stringify(webhookSpecification)} already exists. Skipping creation and refreshing.`);
-            await refreshExistingWebhooks(webhooks, webhookSpecifications, baseID, token);
-          }
-        } else {
-          console.log(`Reached the maximum limit of three webhooks with different specifications. Skipping creation and refreshing for specification ${JSON.stringify(webhookSpecification)}.`);
-          await refreshExistingWebhooks(webhooks, webhookSpecifications, baseID, token);
-        }
-      }
-  
-      return webhookResults;
-    } catch (error: any) {
-      console.error("Error managing webhooks:", error.message);
-      throw error;
-    }
-  }
-  
-  async function refreshExistingWebhooks(
-    existingWebhooks: any,
-    webhookSpecifications: WebhookSpecification[],
-    baseID: string,
-    token: string
-  ): Promise<void> {
-    for (const webhookSpecification of webhookSpecifications) {
-      const existingWebhook = existingWebhooks.webhooks?.find((webhook: any) =>
-        isEqual(webhook.specification, webhookSpecification)
-      );
-  
-      if (existingWebhook) {
-      
-        const refreshRes = await refreshWebhook(baseID, token, existingWebhook.id);
-        console.log(`Webhook with specification ${JSON.stringify(webhookSpecification)} refreshed.`);
-      }
-    }
-  }
 
+async function manageWebhooks(
+  webhookSpecifications: WebhookSpecification[],
+  notificationURL: string,
+  token: string,
+  baseID: string
+): Promise<any> {
+  try {
+    const webhooks = await fetchWebhooks(baseID, token);
+    const webhookResults: any[] = [];
+    const existingWebhookSpecifications = new Set<string>();
+
+    for (const webhook of webhooks.webhooks || []) {
+      existingWebhookSpecifications.add(JSON.stringify(webhook.specification));
+    }
+
+    for (const webhookSpecification of webhookSpecifications) {
+      if (uniqueWebhookCount > 3) {
+        console.log(
+          "Reached the maximum limit of three webhooks. Skipping creation and refreshing."
+        );
+        await refreshExistingWebhooks(
+          webhooks,
+          webhookSpecifications,
+          baseID,
+          token
+        );
+        break;
+      }
+
+      if (existingWebhookSpecifications.size <= 3) {
+        if (
+          !existingWebhookSpecifications.has(
+            JSON.stringify(webhookSpecification)
+          )
+        ) {
+          const createRes = await createWebhook(
+            baseID,
+            token,
+            notificationURL,
+            webhookSpecification
+          );
+          webhookResults.push(createRes);
+          existingWebhookSpecifications.add(
+            JSON.stringify(webhookSpecification)
+          );
+          uniqueWebhookCount++;
+        } else {
+          console.log(
+            `Webhook with specification ${JSON.stringify(
+              webhookSpecification
+            )} already exists. Skipping creation and refreshing.`
+          );
+          await refreshExistingWebhooks(
+            webhooks,
+            webhookSpecifications,
+            baseID,
+            token
+          );
+        }
+      } else {
+        console.log(
+          `Reached the maximum limit of three webhooks with different specifications. Skipping creation and refreshing for specification ${JSON.stringify(
+            webhookSpecification
+          )}.`
+        );
+        await refreshExistingWebhooks(
+          webhooks,
+          webhookSpecifications,
+          baseID,
+          token
+        );
+      }
+    }
+
+    return webhookResults;
+  } catch (error: any) {
+    console.error("Error managing webhooks:", error.message);
+    throw error;
+  }
+}
+
+async function refreshExistingWebhooks(
+  existingWebhooks: any,
+  webhookSpecifications: WebhookSpecification[],
+  baseID: string,
+  token: string
+): Promise<void> {
+  for (const webhookSpecification of webhookSpecifications) {
+    const existingWebhook = existingWebhooks.webhooks?.find((webhook: any) =>
+      isEqual(webhook.specification, webhookSpecification)
+    );
+
+    if (existingWebhook) {
+      const refreshRes = await refreshWebhook(
+        baseID,
+        token,
+        existingWebhook.id
+      );
+      console.log(
+        `Webhook with specification ${JSON.stringify(
+          webhookSpecification
+        )} refreshed.`
+      );
+    }
+  }
+}
 
 function mapAirtableTypeToPostgresType(airtableType: string): string {
   switch (airtableType) {
@@ -413,23 +470,18 @@ function mapAirtableTypeToPostgresType(airtableType: string): string {
 
 async function main() {
   const baseID = "appff8HJFMBuoXgwd";
-  const tName = "Furniture";
+
   const token =
     "patwI1yOnv2nlWULt.2d3d51fcb8653f64bac88467c64e063ae1eafbfdfd67823f60a9a0d3be13183c";
 
   const pgUrl =
     "postgres://latipovabdukhamid:dUGmSY9fC1gc@ep-long-snowflake-366579.us-east-2.aws.neon.tech/neondb";
-  const tables:any[] = await fetchTables(token,baseID)
-  await createPostgresTable(pgUrl,tables,  tName);
-  await insertRecordsToPostgres(token, baseID, tName, pgUrl);
-  
-
+  await createAllTablesAndInsertRecords(token, baseID, pgUrl);
   const webhookSpecifications: WebhookSpecification[] = [
     {
       options: {
         filters: {
           dataTypes: ["tableData"],
-          
         },
       },
     },
@@ -437,7 +489,6 @@ async function main() {
       options: {
         filters: {
           dataTypes: ["tableFields"],
-          
         },
       },
     },
@@ -445,26 +496,38 @@ async function main() {
       options: {
         filters: {
           dataTypes: ["tableMetadata"],
-          
         },
       },
     },
   ];
-  const webhooks = await manageWebhooks(
+  await manageWebhooks(
     webhookSpecifications,
     "https://6f8e-213-230-112-222.ngrok-free.app/notif-ping",
     token,
     baseID
-  );``
+  );
+  ``;
 
-   for (const webhook of webhooks) {
-    const webhookPayloads = await fetchWebhookPayload(baseID, token, webhook.id);
+  try {
+    const webhooks = await fetchWebhooks(baseID, token);
+    console.log("Webhooks:", webhooks);
 
-    
-    for (const webhookPayload of webhookPayloads) {
-      await updatePostgresTable(pgUrl, webhookPayload,token,baseID);
+    for (const webhook of webhooks.webhooks || []) {
+      console.log("Processing webhook:", webhook);
+      const webhookPayload = await fetchWebhookPayload(
+        baseID,
+        token,
+        webhook?.id
+      );
+      console.log("Webhook payloads", webhookPayload);
+
+      for (const payload of webhookPayload || []) {
+        console.log(webhookPayload);
+        await updatePostgresTable(pgUrl, payload, token, baseID);
+      }
     }
+  } catch (error) {
+    console.log("Error fetching payloads ", error);
   }
 }
-
 main();
